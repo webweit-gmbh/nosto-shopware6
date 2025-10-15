68b41d820d2568434070bb45c3f6c1414fc0f61a
Philip Standt <philip.standt@strix.net>
2025-10-15 17:15:23 +0200

    feat: optimize entity changelog processing

--- src/Service/ScheduledTask/EntityChangelogScheduledTaskHandler.php
+++ src/Service/ScheduledTask/EntityChangelogScheduledTaskHandler.php
@@ -5,9 +5,13 @@ declare(strict_types=1);
 namespace Nosto\NostoIntegration\Service\ScheduledTask;
 
 use Nosto\NostoIntegration\Async\EntityChangelogSyncMessage;
+use Nosto\NostoIntegration\Model\Operation\EntityChangelogSyncHandler;
 use Nosto\Scheduler\Model\Job\GeneratingHandlerInterface;
 use Nosto\Scheduler\Model\JobScheduler;
+use Shopware\Core\Framework\Context;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
 use Shopware\Core\Framework\MessageQueue\ScheduledTask\ScheduledTaskHandler;
 use Shopware\Core\Framework\Uuid\Uuid;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
@@ -18,12 +22,25 @@ class EntityChangelogScheduledTaskHandler extends ScheduledTaskHandler implement
     public function __construct(
         EntityRepository $scheduledTaskRepository,
         private readonly JobScheduler $jobScheduler,
+        private readonly EntityRepository $nostoSchedulerJobRepository,
     ) {
         parent::__construct($scheduledTaskRepository);
     }
 
     public function run(): void
     {
+        $context = Context::createCLIContext();
+        $criteria = (new Criteria())
+            ->addFilter(new EqualsFilter('type', EntityChangelogSyncHandler::HANDLER_CODE))
+            ->addFilter(new EqualsFilter('status', 'pending'))
+            ->addFilter(new EqualsFilter('parentId', null))
+            ->setLimit(1);
+
+        // Only dispatch a new task if there is none pending
+        if ($this->nostoSchedulerJobRepository->searchIds($criteria, $context)->firstId()) {
+            return;
+        }
+
         $jobMessage = new EntityChangelogSyncMessage(Uuid::randomHex());
         $this->jobScheduler->schedule($jobMessage);
     }
